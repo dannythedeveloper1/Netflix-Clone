cb27ef0b0fd86c14c821fc2ee35047c5641ff526
dannythedeveloper1 <74397260+dannythedeveloper1@users.noreply.github.com>
2024-01-05 15:39:41 -0500

    initial commit

---
+++ netflix-2024/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+//Tvshow 'https://api.themoviedb.org/3/tv/popular?language=en-US&page=1'
+
+export default App;
